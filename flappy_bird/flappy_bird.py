729a4db435d2c046c3298f0c615d4edfa7a48cd5
Ing. Di Filippo <davidedifilippo@gmail.com>
2022-06-22 07:15:44 +0200

    Update flappy_bird.py
    Aggiunti i tubi da evitare, l'inclinazione di volo e fine gioco se si colpisce il terreno.

--- flappy_bird/flappy_bird.py
+++ flappy_bird/flappy_bird.py
@@ -1,6 +1,7 @@
 import pygame
 from bird import Bird
-
+from pipe import Pipe
+import random
 
 clock = pygame.time.Clock()
 
@@ -10,6 +11,13 @@ pygame.init()
 
 screen_width = 864
 screen_height = 768 + 168
+pipe_gap = 150
+pipe_frequency = 1500  # milliseconds
+last_pipe = pygame.time.get_ticks()
+on_ground = True
+
+scroll_ground = 0
+scroll_speed = 4
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy bird")
@@ -18,12 +26,15 @@ bg = pygame.image.load("img/bg.png")
 ground = pygame.image.load("img/ground.png")
 
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 
-flappy = Bird(100, int(screen_height/2))
+flappy = Bird(100, int(screen_height / 2))
+g_pipe = Pipe(400, int(screen_height / 2 + pipe_gap / 2), on_ground, scroll_speed)
+c_pipe = Pipe(400, int(screen_height / 2 - pipe_gap / 2), not on_ground, scroll_speed)
+
 bird_group.add(flappy)
-
-scroll_ground = 0
-scroll_speed = 4
+pipe_group.add(g_pipe)
+pipe_group.add(c_pipe)
 
 run = True
 
@@ -34,6 +45,16 @@ while run:
     bird_group.draw(screen)
     bird_group.update()
 
+    now = pygame.time.get_ticks()
+    if now - last_pipe > pipe_frequency:
+        pipe_height = random.randint(100, 300)
+        g_pipe = Pipe(screen_width, int(screen_height / 2 + pipe_height / 2), on_ground, scroll_speed)
+        c_pipe = Pipe(screen_width, int(screen_height / 2 - pipe_height / 2), not on_ground, scroll_speed)
+        pipe_group.add(g_pipe)
+        pipe_group.add(c_pipe)
+        last_pipe = pygame.time.get_ticks()
+    pipe_group.draw(screen)
+    pipe_group.update()
     screen.blit(ground, (scroll_ground, 768))
     scroll_ground -= scroll_speed
 
@@ -42,5 +63,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    if flappy.rect.bottom > 768:
+        run = False
     pygame.display.update()
 pygame.quit()
