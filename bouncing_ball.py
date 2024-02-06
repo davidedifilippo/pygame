d52af0bbd2dbcb50307b870cffc51fe2246c66cf
Ing. Di Filippo <davidedifilippo@gmail.com>
2024-02-06 18:30:25 +0100

    Update bouncing_ball.py
    Aggiornata la gestione dei frame rate. Precedentemente il movimento dipendeva dagli fps della macchina

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -1,30 +1,51 @@
-import sys, pygame
+import sys
+import pygame
+
 pygame.init()
 
-size = width, height = 1000, 1200
-speed = [2, 1]
-black = 50, 10, 0
+# screen size
+width = 800
+height = 600
 
-screen = pygame.display.set_mode(size)
+# make screen
+screen = pygame.display.set_mode((width, height))
 
-ball = pygame.image.load("football-gd5b0bcc66_640.png")
+# load ball image
+ball = pygame.image.load("football.png")
 x, y = ball.get_size()
-sf = 0.2
 
+print('width_Ball in pixel', x)
+print('height_Ball in pixel', y)
 
-ball = pygame.transform.scale(ball, (int(x*sf), int(y*sf)))
-ballrect = ball.get_rect()
+# speed in pixel per frame
+speed = [1, 1]
+black = 100, 10, 0
+
+# object too big
+shrinkFactor = 0.2
+
+ball = pygame.transform.scale(ball, (int(x * shrinkFactor), int(y * shrinkFactor)))
+ball_rect = ball.get_rect()
+
+# The clock provides several functions to help control a game's fps.
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    ball_rect = ball_rect.move(speed)
+    if ball_rect.left < 0 or ball_rect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ball_rect.top < 0 or ball_rect.bottom > height:
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(ball, ball_rect)
     pygame.display.flip()
+
+    # By calling Clock.tick(150) once per frame, the program will never run at more than 150 frames per second.
+
+    clock.tick(150)
+    print(clock.get_fps())
